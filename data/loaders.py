fdf2f7be642f284a7339e63aec50cc43e6a87225
StevRamos <stevhr@gmail.com>
2021-01-27 01:17:35 -0500

    dataloader updated

--- data/loaders.py
+++ data/loaders.py
@@ -13,14 +13,19 @@ class BatchCoSELoader(Dataset):
     def __init__(self, path: str, filenames: dict):
         self.inputs = pickle.load(open(os.path.join(path, filenames["inputs_file"]), 'rb'))
         self.targets = pickle.load(open(os.path.join(path, filenames["targets_file"]), 'rb'))
+
+        for input_index in len(self.inputs):
+            for input_key, input_value in self.inputs[input_index].items():
+                self.inputs[input_index][input_key] = torch.from_numpy(input_value)
+
+        for target_index in len(self.targets)
+            for target_key, target_value in self.targets[target_index].items():
+                self.targets[target_index][target_key] = torch.from_numpy(target_value)
+
         self.len = len(self.inputs)
         assert (len(self.inputs) == len(self.targets))
     
     def __getitem__(self, index):
-        for input_key, input_value in self.inputs[index].items():
-            self.inputs[index][input_key] = torch.from_numpy(input_value)
-        for target_key, target_value in self.targets[index].items():
-            self.targets[index][target_key] = torch.from_numpy(target_value)
         return self.inputs[index], self.targets[index]
     
     def __len__(self):
